cfde126c17acf77579c6890a73a39f746d64cc5a
Dipak <dade.dipakg@gmail.com>
2020-09-07 16:40:55 +0530

    Created houses, complex_building and commerial_units

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :commercial_units
+  resources :complex_buildings
+  resources :houses
   devise_for :users
   root to: "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
